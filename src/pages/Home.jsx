c12514af1898a2d8a734303df137d7a97f33ebf7
gomina maryam <gominamaryam@gmail.com>
2022-02-05 01:36:23 +0100

    add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import MainLayout from "../layouts/MainLayout";
+
+const Home = () => {
+	return (
+		<MainLayout>
+			<h1>Welcome to HRentals</h1>
+		</MainLayout>
+	);
+};
+
+export default Home;
