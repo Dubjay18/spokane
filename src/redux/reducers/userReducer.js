a181056b5fe667aba60fa6a15cbf4aecb8996220
xpanvictor <xpanvictor@gmail.com>
2022-05-31 09:31:36 +0100

    Feat: Added the user reducer

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,18 @@
+import { usertypes } from "../types/actiontypes";
+
+const userReducer = (state, { type, payload }) => {
+  switch (type) {
+    case usertypes.SET_EMAIL:
+      return { ...state, payload };
+    case usertypes.SET_USERNAME:
+      return { ...state, payload };
+    case usertypes.SET_ENTRY:
+      return { ...state, payload };
+    case usertypes.SET_TOKEN:
+      return { ...state, payload };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
